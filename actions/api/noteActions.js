995be70a0dd6b748c90efb6c58379c7e0309400b
kamail12 <kamiloxen1@gmail.com>
2024-04-18 20:49:40 +0200

    Create logic in backend with endpoind and schema validation

--- actions/api/noteActions.js
+++ actions/api/noteActions.js
@@ -2,35 +2,83 @@ const Note = require("../../db/models/note")
 
 module.exports = {
     
-    getAllNotes: function(req, res){
-        //Pobieranie wszystkich notatek
-        res.send("Pobieranie wszystkich notatek")
+    getAllNotes: async function(req, res){
+        let documents;
+        try {
+             documents = await Note.find({});
+        } catch (error) {
+           return res.status(500).json({message: error.message})
+        }
+
+        res.status(200).json(documents);
+
     },
 
-    getNote: function(req, res){
+    getNote: async function(req, res){
         //Pobieranie notatki po id
-        res.send("Pobieranie notatki po id")
+        let note;
+        const id = req.params.id;
+        try{
+            note = await Note.findOne({_id: id})
+            if (!note) {
+                return res.status(404).json({message: 'Notatka nie została znaleziona'})
+            }
+        }catch(error){
+            return res.status(500).json({message: error.message})
+        }
+
+
+        res.status(200).json(note);
     },
 
-    saveNote: function(req, res){
-
+    saveNote: async function(req, res){
         const title = req.body.title
         const body = req.body.body
-        
+        let note;
+
+        try{
+            note = new Note({title,body,})
+            note = await note.save()
+        }catch(error){
+            return res.status(500).json({message: error.message})
+        }
 
         res.send(`Notatka została stworzona jej tytuł to ${title} a treść to ${body}`)
     },
 
-    updateNote: function(req, res){
-        //Aktualizacja notatki
-        res.send("Aktualizacja notatki")
+    updateNote: async function(req, res){
+        const id = req.params.id
+        const title = req.body.title
+        const body = req.body.body
+        let note;
+
+        try{
+            note = await Note.findOne({_id: id})
+            note.title = title;
+            note.body = body;
+            await note.save();
+        }catch(error){
+            return res.status(500).json({message: error.message})
+            
+        }
+
+
+        res.send("Notatka została zakutlizowana a jej treść to: " + note)
     },
 
-    deleteNote: function(req, res){
+    deleteNote: async function(req, res){
         const id = req.params.id
-
-        //Usuwanie notatki
-        res.send("Usuwanie notatki o id: " + id)
+        let note;
+        
+        try{
+            note = await Note.findOneAndDelete({_id: id})
+            if (!note) {
+                return res.status(404).json({message: 'Notatka nie została znaleziona'})
+            }
+            res.send("Notatka została usunięta")
+        }catch(error){
+            return res.status(500).json({message: error.message})
+        }
     }
 }
 
